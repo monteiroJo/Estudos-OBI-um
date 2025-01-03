05442f2282d88c6831387289a869bd70fe0354cd
unknown <thiagomonteiro81944@gmail.com>
2025-01-03 19:23:07 -0300

    adição de 'calculo rápido' 2021f2

---
+++ nivel2/obi2021/fase2/calculo.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class calculo {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk;
+
+        int[] todasSomasDigitos = new int[10001];
+        for (int i = 0; i < todasSomasDigitos.length; i++) {
+            int somaDigitos = 0;
+            for (int j = i; j > 0; j /= 10) {
+                somaDigitos += j % 10;
+            }  
+            todasSomasDigitos[i] = somaDigitos;  
+        }
+
+        tk = new StringTokenizer(in.readLine());
+        int s = Integer.parseInt(tk.nextToken());
+        tk = new StringTokenizer(in.readLine());
+        int a = Integer.parseInt(tk.nextToken());
+        tk = new StringTokenizer(in.readLine());
+        int b = Integer.parseInt(tk.nextToken());
+
+        int res = 0;
+        for (int i = a; i <= b; i++) {
+            if (todasSomasDigitos[i] == s) res++;
+        }
+        System.out.println(res);
+
+    }    
+}
